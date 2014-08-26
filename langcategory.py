9bebdd806fd7b345d5c0f9e10012349e8b52827b
CNBorn <CNBorn@hotmail.com>
2014-08-26 20:05:23 +0800

    init commit

---
+++ langcategory.py
@@ -0,0 +1,48 @@
+#coding: utf-8
+from operator import attrgetter, itemgetter
+from collections import defaultdict
+
+from pelican import signals
+from pelican.urlwrappers import URLWrapper
+
+class Language(URLWrapper):
+    def __init__(self, name, *args, **kwargs):
+        super(Language, self).__init__(name.strip(), *args, **kwargs)
+        self.lang = self.name
+
+def generate_lang_as_category(generator, writer):
+    self = generator
+    self.languages = defaultdict(list)
+
+    template = self.get_template('category')
+
+    if not self.settings['LANGUAGE_SAVE_AS']:
+        return
+
+    for article in self.articles + self.translations:
+        self.languages[article.lang].append(article)
+
+    for lang, items in self.languages.items():
+        lang_wrapper = Language(lang, self.settings)
+        items.sort(key=attrgetter('date'), reverse=True)
+        dates = [article for article in self.dates if article in items]
+
+        # Override Default Language Setting
+        override_in_default_lang_items = []
+        for i in items:
+            i.in_default_lang = True
+            override_in_default_lang_items.append(i)
+        items = override_in_default_lang_items
+
+        writer.write_file(lang_wrapper.save_as,
+              template,
+              self.context, 
+              articles=items,
+              dates=dates,
+              paginated={'articles': items, 'dates': dates},
+              page_name=lang_wrapper.page_name,
+              all_articles=items
+          )
+
+def register():
+    signals.article_writer_finalized.connect(generate_lang_as_category)
