94fc0bc2983e92d954708611b36448724bf71616
CNBorn <CNBorn@hotmail.com>
2014-08-28 08:30:55 +0800

    remove tailing slash automatically

--- langcategory.py
+++ langcategory.py
@@ -1,9 +1,11 @@
 #coding: utf-8
+import logging
 from operator import attrgetter, itemgetter
 from collections import defaultdict
 
 from pelican import signals
 from pelican.urlwrappers import URLWrapper
+
 
 class Language(URLWrapper):
     def __init__(self, name, *args, **kwargs):
@@ -19,6 +21,13 @@ def generate_lang_as_category(generator, writer):
     if not self.settings['LANGUAGE_SAVE_AS']:
         return
 
+    # If LANGUAGE_URL has a trailing slash, remove it and provide a warning
+    logger = logging.getLogger(__name__)
+    langurl = self.settings['LANGUAGE_URL']
+    if (langurl.endswith('/')):
+        self.settings['LANGUAGE_URL'] = langurl[:-1]
+        logger.warning("Removed extraneous trailing slash from LANGUAGE_URL.")
+
     for article in self.articles + self.translations:
         self.languages[article.lang].append(article)
 
@@ -28,8 +37,10 @@ def generate_lang_as_category(generator, writer):
         dates = [article for article in self.dates if article in items]
 
         # Override Default Language Setting
+        self.settings['DEFAULT_LANG'] = lang
         override_in_default_lang_items = []
         for i in items:
+            i.lang = lang
             i.in_default_lang = True
             override_in_default_lang_items.append(i)
         items = override_in_default_lang_items
